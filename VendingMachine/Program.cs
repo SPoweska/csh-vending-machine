726b59526794d1a9e77fdff19bb39cf3bf30efca
Sebastian <spoweska98@gmail.com>
2020-01-07 16:17:23 +0100

    Added startMachine method

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -9,12 +9,58 @@ namespace VendingMachine
 {
     class Program
     {
+        private static List<float> acceptedNominals;
+        private const string welcomeText = "Siema, siema! O tej porze każdy wypić może, wiec dej no 2 złote!";
+        private const string nominals = "Akceptowane nominały: 50gr, 1zł, 2zł, 5zł";
+        private const string notAcceptedNominal = "Włożono nie akceptowalny nominał";
+
         static void Main(string[] args)
         {
-            EmployeesDatabase emp = new EmployeesDatabase();
-            Console.WriteLine(emp.FetchEmployeeById(1).Name);
-            ProductsDatabase prod = new ProductsDatabase();
-            Console.WriteLine(prod.FetchProductById(2).Name);
+            acceptedNominals = new List<float>();
+            acceptedNominals.Add(0.5f);
+            acceptedNominals.Add(1);
+            acceptedNominals.Add(2);
+            acceptedNominals.Add(5);
+            StartMachine();
+        }
+
+        private static void StartMachine()
+        {
+            bool end = false;
+            do {
+                Output(welcomeText);
+                Output(nominals);
+                float money;
+                try
+                {
+                    money = float.Parse(Input());
+                }
+                catch (System.FormatException ex)
+                {
+                    continue;
+                }
+                bool accepted = false;
+                foreach(var price in acceptedNominals)
+                {
+                    if (price == money)
+                        accepted = true;
+                }
+                if (accepted == false)
+                {
+                    Output(notAcceptedNominal);
+                    continue;
+                }
+            } while (!end);
+            Pause();
+        }
+
+        private static string Input()
+        {
+            return Console.ReadLine();
+        }
+
+        private static void Pause()
+        {
             Console.ReadKey();
         }
 
@@ -22,7 +68,7 @@ namespace VendingMachine
         /// Shows given text to the console
         /// </summary>
         /// <param name="text">text to show</param>
-        private void Output(string text)
+        public static void Output(string text)
         {
             Console.WriteLine(text);
         }
